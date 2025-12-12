6beb620996e02ccc166c079341d4706d3c876a57
Manjunath Beli <belibug@outlook.com>
2025-12-12 14:06:49 -0600

    with env

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Data logger with simple format.
 
+## Setup
+
+By default jsonl (json per line) database is saved in home directory in PowerNotes sub directory. Set `env` variable `PowerNotesDir` with valid folder to change the default location. 
+
 ### Workflow
 
 - Use `New-Note` to create note, it accepts topic (optional) and body
--- src/private/GetNotesDir.ps1
+++ src/private/GetNotesDir.ps1
@@ -1,14 +1,22 @@
 function Get-NotesDir {
-    if ($IsWindows) {
-        $base = $env:APPDATA
-    } elseif ($IsLinux -or $IsMacOS) {
-        $base = $env:XDG_DATA_HOME
-        if (-not $base) {
-            if ($IsMacOS) { $base = Join-Path $HOME 'Library/Application Support' }
-            else { $base = Join-Path $HOME '.local/share' }
+
+    if ($env:PowerNotesDir) {
+        if (Test-Path -PathType Container -Path $env:PowerNotesDir) {
+            $dir = $env:PowerNotesDir
         }
-    } else { throw 'Unsupported OS' }
-    $dir = Join-Path $base 'PowerNotes'
+    } else {
+        ## If environment is not set, assign default path based on OS
+        if ($IsWindows) {
+            $base = $env:APPDATA
+        } elseif ($IsLinux -or $IsMacOS) {
+            $base = $env:XDG_DATA_HOME
+            if (-not $base) {
+                if ($IsMacOS) { $base = Join-Path $HOME 'Library/Application Support' }
+                else { $base = Join-Path $HOME '.local/share' }
+            }
+        } else { throw 'Unsupported OS' }
+        $dir = Join-Path $base 'PowerNotes'
+    }
     if (-not (Test-Path $dir)) { New-Item -ItemType Directory -Path $dir -Force | Out-Null }
     return $dir
 }
