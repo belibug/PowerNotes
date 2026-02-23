1da8f9d5e43e5d808654bab181d282e13f913a96
Manjunath Beli <belibug@outlook.com>
2026-02-23 12:47:36 -0600

    Test fix

--- tests/OutFiles.Tests.ps1
+++ tests/OutFiles.Tests.ps1
@@ -1,18 +1,15 @@
 BeforeAll {
-    $ProjectInfo = Get-MTProjectInfo
-    $PsFiles = Get-ChildItem -LiteralPath $ProjectInfo.OutputModuleDir -Recurse -File |
-    Where-Object { $_.Extension -in '.ps1', '.psm1', '.psd1' } 
+    $data = Get-MTProjectInfo
+    $files = Get-ChildItem $data.OutputModuleDir
 }
 
-
-Describe "PowerShell parse validation ($disOutDir)" {
-
-    It 'parses cleanly: <FullName>' -ForEach $PsFiles {
-        $errors = $null
-        $content = Get-Content -LiteralPath $_.FullName -Raw -ErrorAction Stop
-
-        $null = [System.Management.Automation.PSParser]::Tokenize($content, [ref]$errors)
-
-        $errors.Count | Should -Be 0
+Describe 'Module and Manifest testing' {
+    Context 'Test <_.Name>' -ForEach $files {
+        It 'is valid PowerShell Code' {
+            $psFile = Get-Content -Path $_ -ErrorAction Stop
+            $errors = $null
+            $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            $errors.Count | Should -Be 0
+        }
     }
 }
